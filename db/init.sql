9c42ba3a130dee2bddf60e07804dede5dabf33b1
George Gale <gmgale@icloud.com>
2023-02-11 09:44:51 +0100

    Fixed db init script & Added Make rule.

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,10 @@
 CREATE USER docker;
 CREATE DATABASE docker;
 GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+
+\connect docker;
 -- TODO: Fix why below line works manually but doesnt load here
-CREATE TABLE IF NOT EXISTS wallets (address VARCHAR(100), PRIMARY KEY (address));
+CREATE TABLE IF NOT EXISTS wallets (
+  id              SERIAL PRIMARY KEY,
+  address         VARCHAR(100) NULL
+  );
--- makefile
+++ makefile
@@ -1,2 +1,5 @@
 clean:
 	docker-compose down --volumes
+
+cbu:
+	docker-compose down --volumes && docker-compose build --no-cache && docker-compose up
