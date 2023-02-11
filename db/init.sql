d736f77c1fe6279150b9190b08563652353fb9db
George Gale <gmgale@icloud.com>
2023-02-11 11:35:19 +0100

    Updated db address col size to match max length of xrpl address.

--- db/init.sql
+++ db/init.sql
@@ -6,5 +6,5 @@ GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
 -- TODO: Fix why below line works manually but doesnt load here
 CREATE TABLE IF NOT EXISTS wallets (
   id              SERIAL PRIMARY KEY,
-  address         VARCHAR(100) NULL
+  address         VARCHAR(35) NULL
   );
